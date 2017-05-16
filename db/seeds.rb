b984bab57cfbbdcaac5a6c40bdd43bb898cba066
(Brett Theroux) bretttheroux@gmail.com <bretttheroux@gmail.com>
2017-05-16 15:32:32 +0200

    exercise 2 - model, controller & seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article = Article.new(
+    name: Faker::Commerce.product_name,
+    content: Faker::Lorem.paragraphs(10),
+  )
+  article.save
+end
